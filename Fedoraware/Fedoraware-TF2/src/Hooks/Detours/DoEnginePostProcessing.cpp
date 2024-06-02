94cc65ebef9fff9f40f15cb5a6c02490bf97827b
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:24 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/DoEnginePostProcessing.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/DoEnginePostProcessing.cpp
@@ -1,8 +1,5 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(DoEnginePostProcessing, S::DoEnginePostProcessing(), void, __cdecl,
-	int x, int y, int w, int h, bool bFlashlightIsOn, bool bPostVGui)
-{
-	if (!Vars::Visuals::Removals::PostProcessing.Value || Vars::Visuals::UI::CleanScreenshots.Value && I::EngineClient->IsTakingScreenshot())
-		Hook.Original<FN>()(x, y, w, h, bFlashlightIsOn, bPostVGui);
-}
+// Use a typedef to make the hook declaration easier to read and write
+typedef void(__cdecl *DoEnginePostProcessing_t)(int x, int y, int w, int h, bool bFlashlightIsOn, bool bPostVGui);
+
