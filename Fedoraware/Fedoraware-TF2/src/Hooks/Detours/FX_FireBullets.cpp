445d93f2851f5034ce757fd5038f249daaba7b90
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:04 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/FX_FireBullets.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/FX_FireBullets.cpp
@@ -1,31 +1,26 @@
 #include "../Hooks.h"
 #include "../../Features/Backtrack/Backtrack.h"
 #include "../../Features/Resolver/Resolver.h"
-#include"../../Features/NoSpread/NoSpreadHitscan/NoSpreadHitscan.h"
+#include "../../Features/NoSpread/NoSpreadHitscan/NoSpreadHitscan.h"
 
 namespace S
 {
-	MAKE_SIGNATURE(C_TFWeaponBaseGun_FireBullet_Call, CLIENT_DLL, "83 C4 ? 5F 5E 5B 8B E5 5D C2 ? ? CC CC CC CC 53", 0x0);
+	constexpr auto C_TFWeaponBaseGun_FireBullet_Signatur = Hooks::Signature("83 C4 ? 5F 5E 5B 8B E5 5D C2 ? ? CC CC CC CC 53");
 }
 
-MAKE_HOOK(FX_FireBullets, S::FX_FireBullets(), void, __cdecl,
-	void* pWpn, int iPlayer, const Vec3& vecOrigin, const Vec3& vecAngles, int iWeapon, int iMode, int iSeed, float flSpread, float flDamage, bool bCritical)
+void __fastcall FX_FireBullets_Hook(void* pEcx, void* , int iPlayer, const Vec3& vecOrigin, const Vec3& vecAngles, int iWeapon, int iMode, int iSeed, float flSpread, float flDamage, bool bCritical)
 {
-	static auto dwFireBulletCall = S::C_TFWeaponBaseGun_FireBullet_Call();
-	const auto dwRetAddr = reinterpret_cast<DWORD>(_ReturnAddress());
-
 	if (iPlayer != I::EngineClient->GetLocalPlayer())
 	{
-		//Utils::ConLog("FX_FireBullets", tfm::format("%d {%.1f, %.1f, %.1f}", iPlayer, vecAngles.x, vecAngles.y, vecAngles.z).c_str(), {0, 222, 255, 255});
 		F::Backtrack.ReportShot(iPlayer);
 		F::Resolver.FXFireBullet(iPlayer, vecAngles);
 	}
 
-	if (dwRetAddr != dwFireBulletCall)
+	if (reinterpret_cast<uintptr_t>(_ReturnAddress()) != reinterpret_cast<uintptr_t>(S::C_TFWeaponBaseGun_FireBullet_Signatur))
 		return;
 
 	if (Vars::Aimbot::General::NoSpread.Value)
 		iSeed = F::NoSpreadHitscan.iSeed;
 
-	return Hook.Original<FN>()(pWpn, iPlayer, vecOrigin, vecAngles, iWeapon, iMode, iSeed, flSpread, flDamage, bCritical);
+	return Hook.Original<FN>()(pEcx, iPlayer, vecOrigin, vecAngles, iWeapon, iMode, iSeed, flSpread, flDamage, bCritical);
 }
