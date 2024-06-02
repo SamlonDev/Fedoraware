8226bf0c7efcd46fd23922010ee9d54781c9c19e
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:47 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/S_StartDynamicSound.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/S_StartDynamicSound.cpp
@@ -1,9 +1,12 @@
 #include "../Hooks.h"
 #include "../../Features/Visuals/Visuals.h"
 
-MAKE_HOOK(S_StartDynamicSound, S::S_StartDynamicSound(), int, __cdecl,
-	StartSoundParams_t& params)
+// Define the hook for the S_StartDynamicSound function
+MAKE_HOOK(S_StartDynamicSound, S::S_StartDynamicSound, int, __cdecl, StartSoundParams_t& params)
 {
-	F::Visuals.ManualNetwork(params);
-	return Hook.Original<FN>()(params);
+    // Use the Visuals feature to manually network the sound parameters
+    F::Visuals.ManualNetwork(params);
+
+    // Call the original function with the provided parameters
+    return Hook.Original<FN>()(params);
 }
