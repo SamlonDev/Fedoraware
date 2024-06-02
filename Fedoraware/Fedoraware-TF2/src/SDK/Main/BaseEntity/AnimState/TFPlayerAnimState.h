5a5d84dd74e6b31ff2627d6ceae3b9b04c0716a1
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:13 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/SDK/Main/BaseEntity/AnimState/TFPlayerAnimState.h
+++ Fedoraware/Fedoraware-TF2/src/SDK/Main/BaseEntity/AnimState/TFPlayerAnimState.h
@@ -1,32 +1,52 @@
 #pragma once
 #include "MultiPlayerAnimState.h"
 
-class CTFPlayerAnimState : public CMultiPlayerAnimState
+class ICTFPlayerAnimStateListener
 {
 public:
-	CBaseEntity* GetTFPlayer() { return m_pTFPlayer; }
+    virtual void OnClearAnimationState() = 0;
+    // Add other event handlers as needed
+};
 
-	virtual void		ClearAnimationState() = 0;
-	virtual Activity	TranslateActivity(Activity actDesired) = 0;
-	virtual void		Update(float eyeYaw, float eyePitch) = 0;
-	virtual void		CheckStunAnimation() = 0;
-	virtual Activity	CalcMainActivity() = 0;
-	virtual void		ComputePoseParam_AimYaw(CStudioHdr* pStudioHdr) = 0;
-	virtual float		GetCurrentMaxGroundSpeed() = 0;
-	virtual float		GetGesturePlaybackRate() = 0;
-	virtual bool		ShouldUpdateAnimState() = 0;
-	virtual void		GetOuterAbsVelocity(Vec3& vel) = 0;
-	virtual void		RestartGesture(int iGestureSlot, Activity iGestureActivity, bool bAutoKill = true) = 0;
-	void				SetRenderAngles(const Vec3& angles) { m_angRender = angles; }
+class CTFPlayerAnimState : private CMultiPlayerAnimState
+{
+public:
+    CTFPlayerAnimState(CBaseEntity* pTFPlayer);
+    virtual ~CTFPlayerAnimState();
 
-	CBaseEntity* m_pTFPlayer;
-	bool		 m_bInAirWalk;
-	float		 m_flHoldDeployedPoseUntilTime;
-	float		 m_flTauntMoveX;
-	float		 m_flTauntMoveY;
-	float		 m_flVehicleLeanVel;
-	float		 m_flVehicleLeanPos;
-	Vec3		 m_vecSmoothedUp;
+    CBaseEntity* GetTFPlayer() const { return m_pTFPlayer; }
+
+    virtual void ClearAnimationState() override;
+    virtual Activity TranslateActivity(Activity actDesired) override;
+    virtual void Update(float eyeYaw, float eyePitch) override;
+    virtual void CheckStunAnimation() override;
+    virtual Activity CalcMainActivity() override;
+    virtual void ComputePoseParam_AimYaw(CStudioHdr* pStudioHdr) override;
+    virtual float GetCurrentMaxGroundSpeed() override;
+    virtual float GetGesturePlaybackRate() override;
+    virtual bool ShouldUpdateAnimState() override;
+    virtual void GetOuterAbsVelocity(Vec3& vel) override;
+    virtual void RestartGesture(int iGestureSlot, Activity iGestureActivity, bool bAutoKill = true) override;
+
+    void SetRenderAngles(const Vec3& angles);
+
+    void AddListener(ICTFPlayerAnimStateListener* pListener);
+    void RemoveListener(ICTFPlayerAnimStateListener* pListener);
+
+private:
+    CBaseEntity* m_pTFPlayer;
+    bool m_bInAirWalk;
+    float m_flHoldDeployedPoseUntilTime;
+    float m_flTauntMoveX;
+    float m_flTauntMoveY;
+    float m_flVehicleLeanVel;
+    float m_flVehicleLeanPos;
+    Vec3 m_vecSmoothedUp;
+
+    std::vector<ICTFPlayerAnimStateListener*> m_Listeners;
+
+    void NotifyClearAnimationState();
+    // Add other helper methods as needed
 };
 
 #define MAXSTUDIOPOSEPARAM 24
