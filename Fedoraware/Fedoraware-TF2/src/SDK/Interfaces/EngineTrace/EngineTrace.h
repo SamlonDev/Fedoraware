4d05a3af1d610e51d3e2865ce83b06ebe11ad567
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:29 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/SDK/Interfaces/EngineTrace/EngineTrace.h
+++ Fedoraware/Fedoraware-TF2/src/SDK/Interfaces/EngineTrace/EngineTrace.h
@@ -1,27 +1,37 @@
 #pragma once
+
+#include <Windows.h>
 #include "../../Includes/Includes.h"
 
-#ifndef ENGINETRACE_H
-#define ENGINETRACE_H
-class CTraceListData;
+#ifdef ENGINETRACE_H
+#	error "engineTrace.h included more than once!"
+#endif
 
+#define ENGINETRACE_H
+
+// Forward declarations
+class CTraceListData;
+class IHandleEntity;
+class CPhysCollide;
+class CGameTrace;
+
+// Interface for enumerating entities
 class IEntityEnumerator
 {
 public:
 	virtual bool EnumEntity(IHandleEntity* pHandleEntity) = 0;
 };
 
-class CPhysCollide;
-
-class CEngineTrace
+// Engine tracing interface
+class IEngineTrace
 {
 public:
-	virtual int GetPointContents(const Vector& vecAbsPosition, IHandleEntity** ppEntity = NULL) = 0;
-	virtual int GetPointContents_Collideable(ICollideable* pCollide, const Vector& vecAbsPosition) = 0;
-	virtual void ClipRayToEntity(const Ray_t& ray, unsigned int fMask, IHandleEntity* pEnt, CGameTrace* pTrace) = 0;
-	virtual void ClipRayToCollideable(const Ray_t& ray, unsigned int fMask, ICollideable* pCollide, CGameTrace* pTrace) = 0;
-	virtual void TraceRay(const Ray_t& ray, unsigned int fMask, CTraceFilter* pTraceFilter, CGameTrace* pTrace) = 0;
+	virtual int GetPointContents(const Vector& vecAbsPosition, IHandleEntity** ppEntity = NULL) const = 0;
+	virtual int GetPointContents_Collideable(ICollideable* pCollide, const Vector& vecAbsPosition) const = 0;
+	virtual void ClipRayToEntity(const Ray_t& ray, unsigned int fMask, IHandleEntity* pEnt, CGameTrace* pTrace) const = 0;
+	virtual void ClipRayToCollideable(const Ray_t& ray, unsigned int fMask, ICollideable* pCollide, CGameTrace* pTrace) const = 0;
+	virtual void TraceRay(const Ray_t& ray, unsigned int fMask, CTraceFilter* pTraceFilter, CGameTrace* pTrace) const = 0;
 };
 
+// Version macro for the interface
 #define VENGINE_TRACE_CLIENT_INTERFACE_VERSION "EngineTraceClient003"
-#endif
