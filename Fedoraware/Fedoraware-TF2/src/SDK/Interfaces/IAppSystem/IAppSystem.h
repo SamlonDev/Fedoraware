55c1f17699f0af8e59d1b6990a218bfce095cb99
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:19 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/SDK/Interfaces/IAppSystem/IAppSystem.h
+++ Fedoraware/Fedoraware-TF2/src/SDK/Interfaces/IAppSystem/IAppSystem.h
@@ -1,18 +1,39 @@
 #pragma once
 
-enum InitReturnVal_t
-{
-	INIT_FAILED = 0,
-	INIT_OK,
-	INIT_LAST_VAL,
-};
+#include <string>
+#include <memory>
+#include <typeindex>
 
 class IAppSystem
 {
 public:
-	virtual bool Connect(CreateInterfaceFn factory) = 0;
-	virtual void Disconnect() = 0;
-	virtual void* QueryInterface(const char* pInterfaceName) = 0;
-	virtual InitReturnVal_t Init() = 0;
-	virtual void Shutdown() = 0;
+    virtual ~IAppSystem() = default;
+
+    virtual bool Connect(CreateInterfaceFn factory) = 0;
+    virtual void Disconnect() = 0;
+
+    template <typename T>
+    T* QueryInterface()
+    {
+        if (auto it = interfaces_.find(typeid(T)); it != interfaces_.end() && it->second == this)
+        {
+            return static_cast<T*>(it->first);
+        }
+
+        return nullptr;
+    }
+
+    virtual InitReturnVal Init() = 0;
+    virtual void Shutdown() = 0;
+
+protected:
+    void AddInterface(const std::type_index& interfaceType, void* interface)
+    {
+        interfaces_[interfaceType] = interface;
+    }
+
+private:
+    std::map<std::type_index, void*> interfaces_;
 };
+
+using IAppSystemPtr = std::unique_ptr<IAppSystem>;
