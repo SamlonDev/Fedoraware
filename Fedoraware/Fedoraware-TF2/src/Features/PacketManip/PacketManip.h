55c287f700d21c46fcc629f8876d6eddd3717a30
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:09 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Features/PacketManip/PacketManip.h
+++ Fedoraware/Fedoraware-TF2/src/Features/PacketManip/PacketManip.h
@@ -1,16 +1,60 @@
 #pragma once
+
+#if !defined(PACKET_MANIP_H)
+#define PACKET_MANIP_H
+
 #include "../Feature.h"
 #include "FakeLag/FakeLag.h"
 #include "AntiAim/AntiAim.h"
 
-class CPacketManip
+class CPacketManip : public Feature
 {
-	bool WillTimeOut();
-	bool AntiAimCheck();
+    bool WillTimeOut();
+    bool AntiAimCheck();
 
 public:
-	void Run(CUserCmd* pCmd, bool* pSendPacket);
+    void Run(CUserCmd* pCmd, bool* pSendPacket) override;
 };
 
 ADD_FEATURE(CPacketManip, PacketManip)
-// (Baan)
+
+#endif //PACKET_MANIP_H
+
+
+#pragma once
+
+#if !defined(FEATURE_H)
+#define FEATURE_H
+
+#include <unordered_map>
+#include <string>
+
+class IFeature
+{
+public:
+    virtual void Run(CUserCmd* pCmd, bool* pSendPacket) = 0;
+};
+
+template <typename T>
+class CFeature : public IFeature
+{
+public:
+    void Run(CUserCmd* pCmd, bool* pSendPacket) override
+    {
+        static_cast<T*>(this)->Run(pCmd, pSendPacket);
+    }
+};
+
+#define ADD_FEATURE(ClassName, FeatureName) std::unordered_map<std::string, IFeature*>& GetFeatures() { \
+    static std::unordered_map<std::string, IFeature*> Features; \
+    return Features; \
+} \
+\
+void RegisterFeature() \
+{ \
+    GetFeatures()[#FeatureName] = new ClassName(); \
+} \
+\
+RegisterFeature();
+
+#endif //FEATURE_H
