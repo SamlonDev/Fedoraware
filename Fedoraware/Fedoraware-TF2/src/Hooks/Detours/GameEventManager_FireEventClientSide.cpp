421c12b75dec1dbe990e8da577131aefaf55f0cd
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:23 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/GameEventManager_FireEventClientSide.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/GameEventManager_FireEventClientSide.cpp
@@ -2,28 +2,30 @@
 
 #include "../../Features/Killstreak/Killstreak.h"
 #include "../../Features/Fedworking/Fedworking.h"
+#include <string>
 
 MAKE_HOOK(GameEventManager_FireEventClientSide, Utils::GetVFuncPtr(I::GameEventManager, 8), bool, __fastcall,
-	void* ecx, void* edx, CGameEvent* pEvent)
+    void* ecx, void* edx, CGameEvent* pEvent)
 {
-	if (!ecx || !edx || !pEvent) { return false; }
-	const FNV1A_t uNameHash = FNV1A::Hash(pEvent->GetName());
-	F::Killstreaker.FireEvents(pEvent, uNameHash);
+    if (!ecx || !edx || !pEvent) { return false; }
 
-	if (uNameHash == FNV1A::HashConst("party_chat"))
-	{
-		const auto msg = pEvent->GetString("text");
+    const FNV1A_t uNameHash = FNV1A::Hash(pEvent->GetName());
+    F::Killstreaker.FireEvents(pEvent, uNameHash);
 
-		// Handle networking
-		if (Utils::StartsWith(msg, "FED@"))
-		{
-			if (Vars::Misc::PartyNetworking.Value)
-			{
-				F::Fedworking.HandleMessage(msg);
-			}
-			return false;
-		}
-	}
+    if (uNameHash == FNV1A::HashConst("party_chat"))
+    {
+        std::string msg = pEvent->GetString("text");
 
-	return Hook.Original<FN>()(ecx, edx, pEvent);
+        // Handle networking
+        if (msg.find("FED@") == 0)
+        {
+            if (Vars::Misc::PartyNetworking.Value)
+            {
+                F::Fedworking.HandleMessage(msg);
+            }
+            return false;
+        }
+    }
+
+    return Hook.Original<FN>()(ecx, edx, pEvent);
 }
