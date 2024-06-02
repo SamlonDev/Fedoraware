25378bdf992a00f0786dc1cb1ab0cfc818f82e4d
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:53 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/SDK/Main/TraceFilters/TraceFilters.h
+++ Fedoraware/Fedoraware-TF2/src/SDK/Main/TraceFilters/TraceFilters.h
@@ -1,26 +1,49 @@
 #pragma once
 #include "../BaseEntity/BaseEntity.h"
 
+class CTraceFilter : public CTraceFilterBase
+{
+public:
+    virtual ETraceType GetTraceType() const = 0;
+};
+
+class CTraceFilterBase
+{
+public:
+    CBaseEntity* pSkip = nullptr;
+
+    virtual bool ShouldHitEntity(void* pEntityHandle, int nContentsMask)
+    {
+        if (pSkip && pSkip == CBaseEntity::GetPointer((IHandleEntity*)pEntityHandle))
+            return false;
+
+        return true;
+    }
+};
+
 class CTraceFilterHitscan : public CTraceFilter
 {
 public:
-	virtual bool ShouldHitEntity(void* pEntityHandle, int nContentsMask);
-	virtual ETraceType GetTraceType() const;
-	CBaseEntity* pSkip = nullptr;
+    virtual ETraceType GetTraceType() const
+    {
+        return TRACE_EVERYTHING;
+    }
 };
 
 class CTraceFilterProjectile : public CTraceFilter
 {
 public:
-	virtual bool ShouldHitEntity(void* pEntityHandle, int nContentsMask);
-	virtual ETraceType GetTraceType() const;
-	CBaseEntity* pSkip = nullptr;
+    virtual ETraceType GetTraceType() const
+    {
+        return TRACE_WORLD_ONLY;
+    }
 };
 
 class CTraceFilterWorldAndPropsOnly : public CTraceFilter
 {
 public:
-	virtual bool ShouldHitEntity(void* pEntityHandle, int nContentsMask);
-	virtual ETraceType GetTraceType() const;
-	CBaseEntity* pSkip = nullptr;
+    virtual ETraceType GetTraceType() const
+    {
+        return TRACE_WORLD_AND_STATICS;
+    }
 };
