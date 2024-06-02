6f85f9891f67705536a2153f3959cb6fe7acbb7b
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:22:13 +0200

    improve

--- Fedoraware/Fedoraware-TF2/include/ImGui/imgui_stdlib.h
+++ Fedoraware/Fedoraware-TF2/include/ImGui/imgui_stdlib.h
@@ -1,12 +1,3 @@
-// dear imgui: wrappers for C++ standard library (STL) types (std::string, etc.)
-// This is also an example of how you may wrap your own similar types.
-
-// Changelog:
-// - v0.10: Initial version. Added InputText() / InputTextMultiline() calls with std::string
-
-// See more C++ related extension (fmt, RAII, syntaxis sugar) on Wiki:
-//   https://github.com/ocornut/imgui/wiki/Useful-Extensions#cness
-
 #pragma once
 
 #include <string>
@@ -15,7 +6,33 @@ namespace ImGui
 {
     // ImGui::InputText() with std::string
     // Because text input needs dynamic resizing, we need to setup a callback to grow the capacity
-    IMGUI_API bool  InputText(const char* label, std::string* str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = nullptr, void* user_data = nullptr);
-    IMGUI_API bool  InputTextMultiline(const char* label, std::string* str, const ImVec2& size = ImVec2(0, 0), ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = nullptr, void* user_data = nullptr);
-    IMGUI_API bool  InputTextWithHint(const char* label, const char* hint, std::string* str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = nullptr, void* user_data = nullptr);
+    IMGUI_API bool InputText(const char* label, std::string* str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = nullptr, void* user_data = nullptr)
+    {
+        // Grow string capacity as needed
+        if (str->capacity() < str->size() + 16)
+            str->resize(str->size() + 16);
+
+        // Use the regular InputText() function
+        return ImGui::InputText(label, &(*str)[0], str->size(), flags, callback, user_data);
+    }
+
+    IMGUI_API bool InputTextMultiline(const char* label, std::string* str, const ImVec2& size, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
+    {
+        // Grow string capacity as needed
+        if (str->capacity() < str->size() + 4 * ImGui::GetTextLineHeight())
+            str->resize(str->size() + 4 * ImGui::GetTextLineHeight());
+
+        // Use the regular InputTextMultiline() function
+        return ImGui::InputTextMultiline(label, &(*str)[0], size, str->size(), flags, callback, user_data);
+    }
+
+    IMGUI_API bool InputTextWithHint(const char* label, const char* hint, std::string* str, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
+    {
+        // Grow string capacity as needed
+        if (str->capacity() < str->size() + 16)
+            str->resize(str->size() + 16);
+
+        // Use the regular InputTextWithHint() function
+        return ImGui::InputTextWithHint(label, hint, &(*str)[0], str->size(), flags, callback, user_data);
+    }
 }
