e3f0f0e56b1a76cadc61fbca5b9bd3941b680b90
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:39 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CClient_Precipitation_SimulateRain.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CClient_Precipitation_SimulateRain.cpp
@@ -1,7 +1,22 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(CClient_Precipitation_SimulateRain, g_Pattern.Find(L"client.dll", L"55 8B EC A1 ? ? ? ? 83 EC 60 F3 0F 10 40 ? 53 57 8B 7D 08 8B D9 F3 0F 5C 47 ? F3 0F 10 8B ? ? ? ? F3 0F 5C C8 0F 57 C0 0F 2F C1 0F 87 ? ? ? ? 8B 07 B9 ? ? ? ? F3 0F 10 4D ? 89 45 F4 0F 28 C1 8B 47 04 F3 0F 59 47"), bool, __fastcall,
-	void* ecx, void* edx, void* pParticle, float dt)
+// Use a typedef to make the hook signature more readable
+typedef bool(*FN_CClient_Precipitation_SimulateRain)(void* ecx, void* edx, void* pParticle, float dt);
+
+// Use a static variable for the hook to make it easier to access from other parts of the code
+static Hook<FN_CClient_Precipitation_SimulateRain> g_hCClient_Precipitation_SimulateRain(
+    g_Pattern.Find(L"client.dll", L"55 8B EC A1 ? ? ? ? 83 EC 60 F3 0F 10 40 ? 53 57 8B 7D 08 8B D9 F3 0F 5C 47 ?"
+                                  L"F3 0F 10 8B ? ? ? ? F3 0F 5C C8 0F 57 C0 0F 2F C1 0F 87 ? ? ? ? 8B 07 B9 ?"
+                                  L"? ? ? ? F3 0F 10 4D ?"));
+
+bool CClient_Precipitation_SimulateRain_Hooked(void* ecx, void* edx, void* pParticle, float dt)
 {
-	return Hook.Original<FN>()(ecx, edx, pParticle, dt);
+    // Do something before the original function is called
+
+    // Call the original function
+    bool result = g_hCClient_Precipitation_SimulateRain.Get()(ecx, edx, pParticle, dt);
+
+    // Do something after the original function is called
+
+    return result;
 }
