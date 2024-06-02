912c2b542e70056a8f10031195a544cebf3211f3
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:20 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CVoiceBanMgr_GetPlayerBan.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CVoiceBanMgr_GetPlayerBan.cpp
@@ -1,9 +1,14 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(CVoiceBanMgr_GetPlayerBan, g_Pattern.Find(L"client.dll", L"55 8B EC FF 75 ? E8 ? ? ? ? 85 C0 0F 95 C0 5D C2 ? ? CC CC CC CC CC CC CC CC CC CC CC CC 55 8B EC 83 EC"), bool, __fastcall,
-	void* ecx, void* edx, char const playerID[SIGNED_GUID_LEN])
+static const auto CVoiceBanMgr_GetPlayerBan_pattern = g_Pattern.Find(L"client.dll", L"55 8B EC FF 75 ? E8 ? ? ? ? 85 C0 0F 95 C0 5D C2 ? ? CC CC CC CC CC CC CC CC CC CC CC 55 8B EC 83 EC");
+if (CVoiceBanMgr_GetPlayerBan_pattern.empty()) {
+    // Log error and return
+}
+
+MAKE_HOOK(CVoiceBanMgr_GetPlayerBan, CVoiceBanMgr_GetPlayerBan_pattern, bool, __fastcall,
+    void* ecx, void* edx, std::string_view playerID)
 {
-	const bool bReturn = Hook.Original<FN>()(ecx, edx, playerID);
-	//Utils::ConLog("CVoiceBanMgr_GetPlayerBan", tfm::format("%s is%s voice-banned.", playerID, (bReturn ? "" : " not")).c_str(), {118, 116, 107, 255});
-	return bReturn;
+    const bool bReturn = Hook.Original<FN>()(ecx, edx, playerID.data());
+    //Utils::ConLog("CVoiceBanMgr_GetPlayerBan", "Player " + std::to_string(bReturn) + " is voice-banned: " + playerID);
+    return bReturn;
 }
