6fb754be7c4dda9b111e7399c6de6e29c4bc81bf
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:23:35 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/NetChannel_SendDatagram.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/NetChannel_SendDatagram.cpp
@@ -2,28 +2,28 @@
 #include "../../Features/Backtrack/Backtrack.h"
 
 MAKE_HOOK(NetChannel_SendDatagram, S::NetChannel_SendDatagram(), int, __fastcall,
-	INetChannel* netChannel, void* edx, bf_write* datagram)
+    INetChannel* netChannel, void* edx, bf_write* datagram)
 {
-	if (!netChannel || datagram)
-		return Hook.Original<FN>()(netChannel, edx, datagram);
+    if (!datagram || netChannel->m_bIsLocalPlayer || netChannel->m_pVoicePlayer)
+        return Hook.Original<FN>()(netChannel, edx, datagram);
 
-	CBaseEntity* pLocal = g_EntityCache.GetLocal();
-	if (!pLocal ||/* !pLocal->IsAlive() || pLocal->IsAGhost() ||*/ !Vars::Backtrack::Enabled.Value || !Vars::Backtrack::Latency.Value)
-	{
-		F::Backtrack.bFakeLatency = false;
-		return Hook.Original<FN>()(netChannel, edx, datagram);
-	}
+    CBaseEntity* pLocal = g_EntityCache.GetLocal();
+    if (!pLocal || !pLocal->IsAlive() || pLocal->IsAGhost())
+    {
+        F::Backtrack.bFakeLatency = false;
+        return Hook.Original<FN>()(netChannel, edx, datagram);
+    }
 
-	F::Backtrack.bFakeLatency = true;
+    F::Backtrack.bFakeLatency = true;
 
-	const int inSequence = netChannel->m_nInSequenceNr;
-	const int inState = netChannel->m_nInReliableState;
+    const int inSequence = netChannel->m_nInSequenceNr;
+    const int inState = netChannel->m_nInReliableState;
 
-	F::Backtrack.AdjustPing(netChannel);
+    F::Backtrack.AdjustPing(netChannel);
 
-	const int original = Hook.Original<FN>()(netChannel, edx, datagram);
-	netChannel->m_nInSequenceNr = inSequence;
-	netChannel->m_nInReliableState = inState;
+    const int original = Hook.Original<FN>()(netChannel, edx, datagram);
+    netChannel->m_nInSequenceNr = inSequence;
+    netChannel->m_nInReliableState = inState;
 
-	return original;
+    return original;
 }
