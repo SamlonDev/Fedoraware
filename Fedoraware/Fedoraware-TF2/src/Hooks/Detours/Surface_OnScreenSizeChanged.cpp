47497b4d5d4d2b89cb0a1b0afd0acb9c40197fa9
Samlon <140019637+SamlonDev@users.noreply.github.com>
2024-06-02 21:22:43 +0200

    improve

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/Surface_OnScreenSizeChanged.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/Surface_OnScreenSizeChanged.cpp
@@ -1,10 +1,18 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(Surface_OnScreenSizeChanged, Utils::GetVFuncPtr(I::MatSystemSurface, 111), void, __fastcall,
-	void* ecx, void* edx, int nOldWidth, int nOldHeight)
-{
-	Hook.Original<FN>()(ecx, edx, nOldWidth, nOldHeight);
+// Use a lambda to wrap the original function and avoid potential issues with function pointers
+auto Surface_OnScreenSizeChanged_Original =
+    [original = Utils::GetVFuncPtr<FN>(I::MatSystemSurface, 111)]() -> void(__fastcall*)(void*, void*, int, int) {
+        return original;
+    }();
 
-	g_ScreenSize.Update();
-	g_Draw.ReloadFonts();
+void Hooked_Surface_OnScreenSizeChanged(void* ecx, void* edx, int nOldWidth, int nOldHeight) {
+    // Call the original function
+    Surface_OnScreenSizeChanged_Original(ecx, edx, nOldWidth, nOldHeight);
+
+    // Call the custom code
+    g_ScreenSize.Update();
+    g_Draw.ReloadFonts();
 }
+
+MAKE_HOOK(Surface_OnScreenSizeChanged, &Hooked_Surface_OnScreenSizeChanged);
